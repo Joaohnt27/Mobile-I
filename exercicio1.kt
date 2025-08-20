eec955a04327d718cd517cf08f7d34590bff6688
João Henrique <126480950+Joaohnt27@users.noreply.github.com>
2025-08-20 19:41:22 -0300

    Create exercicio1.kt

---
+++ exercicio1.kt
@@ -0,0 +1,15 @@
+/* Exercício 1
+ * Declare variáveis para armazenar nome (String), idade (Int) e altura (Double).
+ Implemente a função informacoes() que retorne uma string formatada no estilo:
+ "Nome: X, Idade: Y, Altura: Z m"
+*/
+
+fun main() {
+    println(informacoes("João Henrique", 21, 1.80))
+    // Saída esperada: Nome: João Henrique, Idade: 21, Altura: 1.80 m
+}
+
+
+fun informacoes (nome: String, idade: Int, altura: Double): String {
+    return "Nome: $nome, idade: $idade, altura: $altura"
+}
