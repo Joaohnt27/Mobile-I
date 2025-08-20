f2f3a087079f0f440f4185c94eb70f5b910ef5d6
João Henrique <126480950+Joaohnt27@users.noreply.github.com>
2025-08-20 20:16:48 -0300

    Create exercicio6.kt

---
+++ exercicio6.kt
@@ -0,0 +1,21 @@
+/* Exercício 6
+ * Crie uma interface Imprimivel com o método imprimir(). Crie a classe Relatorio que implemente essa interface e imprima "Gerando relatório...".
+*/
+
+
+fun main() {
+    val r: Imprimivel = Relatorio()
+    r.imprimir() // Saída: Gerando relatório...
+}
+
+interface Imprimivel {
+    fun imprimir()
+}
+
+class Relatorio: Imprimivel {
+    override fun imprimir() {
+        println("Gerando relatório...")
+    }
+}
+
+
